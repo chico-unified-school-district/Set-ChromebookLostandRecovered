4cfac71cc7da261a1af4ab18052f4683264cbcf3
jcooperCUSD <jcooper@chicousd.org>
2019-08-14 15:26:08 -0700

    fixed sql and script logic for new Aeries config

--- sql/query-devices.sql
+++ sql/query-devices.sql
@@ -1,7 +1,7 @@
 SELECT STU.ID,
  [DRA].[SCL] AS [School],
  [STU].[SEM] AS [StuEmail],
- [DRA].[CC] AS [Condition],
+ [DRA].[CD] AS [Code],
  CONVERT(VARCHAR(10),[DRA].[DT],101) AS [Issued],
  CONVERT(VARCHAR(10),[DRA].[RD],101) AS [Returned],
  [DRA].[DD] as [Last Ticket Date],
@@ -11,6 +11,8 @@ FROM (SELECT [STU].* FROM STU WHERE DEL = 0)
  DRA LEFT JOIN (SELECT [DRI].* FROM DRI WHERE DEL = 0)
  DRI ON [DRI].[RID] = [DRA].[RID] AND [DRI].[RIN] = [DRA].[RIN]) ON [STU].[ID] = [DRA].[ID]
 WHERE
+ DRA.CD IN ('M','S','V')
+ AND
  DRA.RID = 1
  AND
  DRI.DEL <> 1
